c3b041044b0281b41c180bf97ae8550174b0229b
Chaitanya <chaitu.vsh@gmail.com>
2020-12-01 00:26:50 +0530

    Changed the variant of the button

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
         <p>You can always find me loving myself... {emojify(":heart:")}</p>
         <p>Life is never only about <code>ctrl C</code> and <code>ctrl V</code></p>
         <br/>
-        <Button variant="outline-secondary" href="https://github.com/ChaitanyaVSH" target="_blank" className="btnModified">Github</Button>
+        <Button variant="outline-warning" href="https://github.com/ChaitanyaVSH" target="_blank" className="btnModified">Github</Button>
         <Button variant="outline-info" href="https://www.linkedin.com/in/kotipalli-chaitanya-977917150/" target="_blank" className="btnModified">LinkedIN</Button>
         <Button variant="outline-success" href="https://www.hackerrank.com/qwertyfellow" target="_blank" className="btnModified">Hackerrank</Button>
         <LineParticles/>
@@ -28,8 +28,9 @@ export default class App extends Component {
     )
   }
   /**
-   * 1. Fix the CSS Bug for smaller screens.
+   * 1. Fix the CSS Bug for smaller screens. DONE
    * 2. Implement the favorite quotes component to be in the center of the screen.
    * 3. Explore Apple Desing system and pick the components styles/layouts.
+   * 4. Use cards to design the projects worked so far. https://react-bootstrap.github.io/components/cards/ || https://react-bootstrap.github.io/components/cards/#title-text-and-links
    */
 }
