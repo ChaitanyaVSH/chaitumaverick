327bc6cc1275dcacae60a312186863d748bf9892
Chaitanya <chaitu.vsh@gmail.com>
2021-12-26 16:24:34 +0530

    Added confetti and basic setup for new year

--- src/components/confetti/confetti.js
+++ src/components/confetti/confetti.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import image from "../../assets/flame-new-year-party-2022.png";
+import Confetti from 'react-confetti'
+import "./confetti.css"
 
-const Confetti = () => {
+const ConfettiComponent = () => {
     return (
-        <div>
-            <h1>Confetti</h1>
+        <div className="confettiContainer">
+            <Confetti
+                width={1200}
+                height={1200}
+            />
+            <div className="wishingMessage">
+                <img src={image} alt="2022"/>
+                <div className="wishesContent">
+                    <h1>Happy new year</h1>
+                    <h5>Let this year bring you only happiness...</h5>
+                    <h5>Let this year you network more and more...</h5>
+                    <h5>Let this year bring you more launch parties...</h5>
+                    <h5>Let this year bring you less SEV 2s...</h5>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Confetti;
+export default ConfettiComponent;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
-import Confetti from "./components/confetti/confetti";
+import ConfettiComponent from "./components/confetti/confetti";
 import NotFound from "./components/error/NotFound";
 
 const routes = (
     <Routes>
-        <Route exact path="/confetti" element={<Confetti />}/>
+        <Route exact path="/happynewyear" element={<ConfettiComponent />}/>
         <Route exact path="/" element={<App />}/>
         <Route exact path="*" element={<NotFound />} />
     </Routes>
