fd7fe940cdcbb9cd1722c1ffecc89542207b143b
Chaitanya <chaitu.vsh@gmail.com>
2021-12-26 17:06:11 +0530

    Added iTyped effects

--- src/components/confetti/confetti.js
+++ src/components/confetti/confetti.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { init } from 'ityped';
 import image from "../../assets/flame-new-year-party-2022.png";
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
 import "./confetti.css"
 
 const ConfettiComponent = () => {
+
+    useEffect(()=>{
+        const itypedTextHandler_CHM = document.querySelector('#chm_ityped_text_handler');
+        init(itypedTextHandler_CHM,
+            {
+                showCursor: false,
+                strings: [
+                    "Let this year bring you only happiness...",
+                    "Let this year you network more and more...",
+                    "Let this year bring you more launch parties...",
+                    "Let this year bring you less SEV 2s..."
+                ],
+                typeSpeed: 50,
+                backSpeed: 10,
+            });
+    },[]);
+
     return (
         <div className="confettiContainer">
             <Confetti
@@ -13,11 +31,8 @@ const ConfettiComponent = () => {
             <div className="wishingMessage">
                 <img src={image} alt="2022"/>
                 <div className="wishesContent">
-                    <h1>Happy new year</h1>
-                    <h5>Let this year bring you only happiness...</h5>
-                    <h5>Let this year you network more and more...</h5>
-                    <h5>Let this year bring you more launch parties...</h5>
-                    <h5>Let this year bring you less SEV 2s...</h5>
+                    <h1>Happy New Year</h1>
+                    <h5 id="chm_ityped_text_handler"></h5>
                 </div>
             </div>
         </div>
